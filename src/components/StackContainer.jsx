15a30bd9b4d8bc8e9440a4b799412004626e8b35
Eyesore123 <joni.putkinen@protonmail.com>
2025-04-15 13:53:38 +0300

    updates

--- src/components/StackContainer.jsx
+++ src/components/StackContainer.jsx
@@ -1,43 +1,68 @@
 import React from 'react';
+import '../styles/styles.css';
 
 export default function StackContainer() {
   return (
     <div className="tech-stack-container">
       <div className="tech-stack-icon">
-        <img src="/php-original.svg" alt="php" />
+        <a href="https://www.php.net/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/php-original.svg" alt="php" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="/astro-plain-wordmark.svg" style={{filter: 'invert(1)'}} alt="Astro" />
+        <a href="https://docs.astro.build/en/concepts/why-astro/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/astro-plain-wordmark.svg" style={{ filter: 'invert(1)' }} alt="Astro" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="/wordpress-plain-wordmark.svg" style={{filter: 'invert(1)'}} alt="Wordpress" />
+        <a href="https://wordpress.org/about/features/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/wordpress-plain-wordmark.svg" style={{ filter: 'invert(1)' }} alt="Wordpress" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="/react-original.svg" alt="React" />
+        <a href="https://react.dev/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/react-original.svg" alt="React" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="/javascript-original.svg" alt="JavaScript" />
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/javascript-original.svg" alt="JavaScript" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="typescript-original.svg" alt="TypeScript" />
+        <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="typescript-original.svg" alt="TypeScript" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="/nodejs-original-wordmark.svg" alt="Node.js" />
+        <a href="https://nodejs.org/en/about" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/nodejs-original-wordmark.svg" alt="Node.js" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="/nextjs-original.svg" alt="NextJS" />
+        <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/nextjs-original.svg" alt="NextJS" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="/tailwindcss-original.svg" alt="TailwindCSS" />
+        <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/tailwindcss-original.svg" alt="TailwindCSS" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="/firebase-original.svg" alt="Firebase" />
+        <a href="https://firebase.google.com/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/firebase-original.svg" alt="Firebase" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-        <img src="/supabase-plain-wordmark.svg" style={{filter: 'invert(1)'}} alt="Supabase" />
+        <a href="https://supabase.com/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/supabase-plain-wordmark.svg" style={{ filter: 'invert(1)' }} alt="Supabase" />
+        </a>
       </div>
       <div className="tech-stack-icon">
-      <img src="/git.svg" alt="GitHub" />
+        <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
+          <img src="/git.svg" alt="GitHub" />
+        </a>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,8 +17,8 @@ export default function Home() {
     </div>
       <div className='hero-text col-span-2 flex flex-col justify-center items-center'>
         <div>
-          <h1 className='gradienttext'>Joni Putkinen</h1>
-          <h6 className='gradienttext flex flex-row justify-end m-0 p-0'>Web Developer / Designer</h6>
+          <h1 className='movingtext'>Joni Putkinen</h1>
+          <h6 className='movingtext flex flex-row justify-end m-0 p-0'>Web Developer / Designer</h6>
           <div className='flex flex-row justify-end items-center'>
           <a href='/projects' className='gradienttext flex flex-row justify-end !mt-20 font-bold' id='projectsbtn'>Recent projects
           </a>
